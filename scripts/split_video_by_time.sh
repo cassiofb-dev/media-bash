157aace59ee3b28e894b77e93221e340cf4502b0
cassiofb-dev <cassiofb.souza@gmail.com>
2022-10-13 16:28:27 -0300

    add "ffmpeg split video by time"

---
+++ scripts/split_video_by_time.sh
@@ -0,0 +1,13 @@
+read -p "Video file: " video_file
+read -p "Split time (hh:mm:ss)" split_time
+
+IFS="."
+
+read -a strarr <<<"$video_file"
+
+file_name="${strarr[0]}"
+file_ext="${strarr[1]}"
+
+codec_str="-c copy -map 0 -segment_time ${split_time} -f segment -reset_timestamps 1"
+
+eval "ffmpeg -i input/${video_file}  ${codec_str} output/${file_name}%03d.${file_ext}"
