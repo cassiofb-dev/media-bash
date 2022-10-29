c01bdf24b1c77bcec81baf48b8ec8730929ec2e9
cassiofb-dev <cassiofb.souza@gmail.com>
2022-10-29 10:50:10 -0300

    simplify

--- scripts/video_ab_av1.sh
+++ scripts/video_ab_av1.sh
@@ -8,8 +8,4 @@ read -a strarr <<<"$video_file"
 
 file_name="${strarr[0]}"
 
-eval "ab-av1 auto-encode -i input/${video_file} --preset 4 -o output/${file_name}.tmp"
-
-eval "ffmpeg -i output/${file_name}.tmp -c:a libopus -b:a 128k -c:v copy output/${file_name}.mkv"
-
-eval "rm output/${file_name}.tmp"
+eval "ab-av1 auto-encode -i input/${video_file} --preset 4 -o output/${file_name}"
