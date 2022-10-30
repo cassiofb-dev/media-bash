8fb9aaf5cf239180470d0e2cec3fee992c9afc34
cassiofb-dev <cassiofb.souza@gmail.com>
2022-10-30 10:59:37 -0300

    add more options

--- scripts/video_stv_av1.sh
+++ scripts/video_stv_av1.sh
@@ -1,5 +1,10 @@
 read -p "Video file: " video_file
 read -p "CRF: " crf
+read -p "Width: " width
+read -p "Height: " height
+read -p "Framerate: " framerate
+
+file_options="crf_${crf}_${width}x${height}_${framerate}fps"
 
 IFS="."
 
@@ -7,6 +12,10 @@ read -a strarr <<<"$video_file"
 
 file_name="${strarr[0]}"
 
+filter_str="-vf scale=${width}:${height},fps=fps=${framerate}"
 codec_str="-c:a copy -c:v libsvtav1 -preset 4 -crf ${crf}"
 
-eval "ffmpeg -i input/${video_file} ${codec_str} output/${file_name}_crf${crf}.mkv"
+eval "ffmpeg -i input/${video_file} ${filter_str} ${codec_str} output/${file_name}_${file_options}_tmp.mkv"
+
+# eval "ffmpeg -i input/${video_file} ${filter_str} output/${file_name}_${file_options}_tmp.mkv"
+# eval "ffmpeg -i output/${file_name}_${file_options}_tmp.mkv ${codec_str}  output/${file_name}_${file_options}.mkv"
