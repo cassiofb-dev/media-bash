dd650af6dc0fb58166c841586637bdb21bbde6a6
cassiofb-dev <cassiofb.souza@gmail.com>
2022-11-16 12:44:11 -0300

    a bit better quality

--- scripts/video_whatsapp_codecs.sh
+++ scripts/video_whatsapp_codecs.sh
@@ -6,6 +6,6 @@ read -a strarr <<<"$video_file"
 
 file_name="${strarr[0]}"
 
-codec_str="-c:a aac -c:v libx264 -preset slow -tune animation -crf 29"
+codec_str="-c:a aac -c:v libx264 -preset slow -tune animation -crf 28"
 
 eval "ffmpeg -i input/${video_file} ${codec_str} output/${file_name}.mp4"
