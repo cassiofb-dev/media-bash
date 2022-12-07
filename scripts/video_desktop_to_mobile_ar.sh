c4a58866d69a6d9a9d2ccd2b239b2f7c034a1e4a
cassiofb-dev <cassiofb.souza@gmail.com>
2022-12-06 22:35:02 -0300

    desktop to mobile ar

---
+++ scripts/video_desktop_to_mobile_ar.sh
@@ -0,0 +1,11 @@
+read -p "Video file: " video_file
+
+IFS="."
+
+read -a strarr <<<"$video_file"
+
+file_name="${strarr[0]}"
+
+codec_str="-c:a copy -c:v libx264 -preset slow -tune animation -crf 20 -vf crop=ih*9/16:ih"
+
+eval "ffmpeg -i input/${video_file} ${codec_str} output/${file_name}.mp4"
