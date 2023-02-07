90db08fb60b020ef771b6957a24e0e09d762f3cb
Cássio Fernando <61878987+cassiofb-dev@users.noreply.github.com>
2023-02-07 01:18:35 -0300

    fix: use m4a file extension

--- scripts/audio_to_aac.sh
+++ scripts/audio_to_aac.sh
@@ -10,4 +10,4 @@ file_name="${strarr[0]}"
 
 codec_str="-c:a aac -b:a 256k"
 
-eval "ffmpeg -i input/${input_file} ${codec_str} output/${file_name}.mp3"
+eval "ffmpeg -i input/${input_file} ${codec_str} output/${file_name}.m4a"
