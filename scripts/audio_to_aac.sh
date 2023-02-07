7ff8f3fd1aacb7bb05f4e290c2959000e61a5fcf
Cássio Fernando <61878987+cassiofb-dev@users.noreply.github.com>
2023-02-07 01:16:28 -0300

    fix: use ffmpeg default aac codec

--- scripts/audio_to_aac.sh
+++ scripts/audio_to_aac.sh
@@ -8,6 +8,6 @@ read -a strarr <<<"$input_file"
 
 file_name="${strarr[0]}"
 
-codec_str="-c:a libfdk_aac -b:a 256k"
+codec_str="-c:a aac -b:a 256k"
 
 eval "ffmpeg -i input/${input_file} ${codec_str} output/${file_name}.mp3"
