5ac5a34cb3da955d559b1f374780c040c93adffd
Cássio Fernando <61878987+cassiofb-dev@users.noreply.github.com>
2023-03-21 19:53:54 -0300

    feat: add more options to whatsapp codec script

--- scripts/video_whatsapp_codecs.sh
+++ scripts/video_whatsapp_codecs.sh
@@ -1,4 +1,6 @@
 read -p "Video file: " video_file
+read -p "Width: " width
+read -p "Height: " height
 
 IFS="."
 
@@ -6,6 +8,7 @@ read -a strarr <<<"$video_file"
 
 file_name="${strarr[0]}"
 
+filter_str="-vf scale=${width}:${height}"
 codec_str="-c:a aac -c:v libx264 -preset slow -tune animation -crf 28"
 
-eval "ffmpeg -i input/${video_file} ${codec_str} output/${file_name}.mp4"
+eval "ffmpeg -i input/${video_file} ${filter_str} ${codec_str} output/${file_name}.mp4"
