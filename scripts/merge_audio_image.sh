56702ce3444cb7cf569189763f85c9c2c907ec5a
cassiofb-dev <cassiofb.souza@gmail.com>
2023-06-17 22:30:50 -0300

    chore: change codec to libx264

--- scripts/merge_audio_image.sh
+++ scripts/merge_audio_image.sh
@@ -5,6 +5,6 @@ read -p "Framerate (number only): " framerate
 
 read -p "Output file: " output_file
 
-codec_str="-c:a libopus -b:a 256K -c:v libsvtav1 -r ${framerate} -shortest"
+codec_str="-c:a libopus -b:a 256K -c:v libx264 -preset slow -crf 20 -r ${framerate} -shortest"
 
 eval "ffmpeg -i input/${audio_file} -loop 1 -i input/${image_file} ${codec_str} output/${output_file}"
