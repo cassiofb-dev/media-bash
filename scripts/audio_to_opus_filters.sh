a0e39fe80224295395355e6da18a766a1bc681d1
cassiofb-dev <cassiofb.souza@gmail.com>
2024-01-21 09:47:03 -0300

    fix: better audio quality

--- scripts/audio_to_opus_filters.sh
+++ scripts/audio_to_opus_filters.sh
@@ -8,7 +8,7 @@ read -a strarr <<<"$input_file"
 
 file_name="${strarr[0]}"
 
-filter_432hz="asetrate=44100*432/440,aresample=44100,atempo=440/432"
+filter_432hz="asetrate=43200,aresample=43200,atempo=1"
 
 filter_slow="atempo=0.865"
 
@@ -20,10 +20,6 @@ filter_normalizer="dynaudnorm"
 
 filter_str="-af ${filter_432hz},${filter_slow},${filter_reverb},${filter_bass},${filter_normalizer}"
 
-eval "ffmpeg -i input/${input_file} ${filter_str} output/${file_name}_tmp.opus"
-
 codec_str="-c:a libopus -b:a 256K"
 
-eval "ffmpeg -i output/${file_name}_tmp.opus ${codec_str} output/${file_name}.opus"
-
-eval "rm output/${file_name}_tmp.opus"
+eval "ffmpeg -i input/${input_file} ${codec_str} ${filter_str} output/${file_name}.opus"
